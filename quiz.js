b32b5b814a43e00795ee6538568a548afa8dd2e5
NeelanjanaGoswami <87737175+NeelanjanaGoswami@users.noreply.github.com>
2022-03-27 13:39:21 -0400

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,6 @@
+addUser(){
+    document.getElementById("player1_name_input").innerHTML = name_1
+    document.getElementById("player2_name_input").innerHTML = name_2
+    localStorage.getItem("player2_name_input")
+    localStorage.getItem("player1_name_input")
+}
